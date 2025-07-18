296688cd05565cf2210029d018488f0c4e2ebb81
Tarun sharma <tarunsharmawee@gmail.com>
2025-07-18 22:34:55 +0530

    Function

--- rust 1/rust-proj/src/main.rs
+++ rust 1/rust-proj/src/main.rs
@@ -1,5 +1,15 @@
 fn main() {
-    for i in 1..101{
-        println!("Ram ram Ladar {}", i)
+    let sentence: String = String::from("Baghi Firse Lotega!");
+    let first_word: String = get_first_word(sentence);
+    println!("First Word of The Sentence is {}",  first_word);
+}
+fn get_first_word(sentence: String) -> String{
+    let mut ans = String::new();
+    for char in sentence.chars(){
+        ans.push_str(char.to_string().as_str());
+        if char == ' ' {
+            break;
+        }
     }
+    return ans
 }
