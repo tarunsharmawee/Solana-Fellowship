eeb04145061bf93589c7c4f819dcc8a76af6ab38
Tarun sharma <tarunsharmawee@gmail.com>
2025-07-19 12:05:08 +0530

    enums with value and pattern matching

--- rust 1/rust-proj/src/main.rs
+++ rust 1/rust-proj/src/main.rs
@@ -1,9 +1,34 @@
-enum Directions {
-    North,
-    East,
-    South,
-    West,
+enum Shape {
+    Circle(f64),  // Variant with associated data (radius)
+    Square(f64),  // Variant with associated data (side length)
+    Rectangle(f64, f64),  // Variant with associated data (width, height)
 }
-fn main(){
-    let my_direction = Directions::North;
+
+// Function to calculate area based on the shape
+fn calculate_area(shape: Shape) -> f64 {
+    // calculates the area of the shape 
+    
+    let ans = match shape {
+        Shape::Circle(radius) => 3.14 * radius * radius,  
+        Shape::Rectangle(width, height) => width * height,
+        Shape::Square(side) => side * side, 
+    };
+    
+    return ans;
+}
+
+fn main() {
+    // Create instances of different shapes
+    let circle = Shape::Circle(5.0);
+    let square = Shape::Square(4.0);
+    let rectangle = Shape::Rectangle(3.0, 6.0);
+
+    let area = calculate_area(circle);
+    println!("Area of the circle: {}", area);
+
+    let area = calculate_area(square);
+    println!("Area of the square: {}", area);
+
+    let area = calculate_area(rectangle);
+    println!("Area of the rectangle: {}", area);
 }
