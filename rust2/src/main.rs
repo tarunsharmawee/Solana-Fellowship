1bd00e2bfee75856107998f0ad4b27b0cba8b193
Tarun sharma <tarunsharmawee@gmail.com>
2025-07-21 01:06:09 +0530

    Iterators

--- rust2/src/main.rs
+++ rust2/src/main.rs
@@ -1,15 +1,9 @@
-use std::collections::HashMap;
+fn  main(){
+    let v1 = vec![1,2,3];
+    let v1_iter = v1.iter();
 
-fn group_value_by_key(vec: Vec<(String, i32)>) -> HashMap<String, i32> {
-    let mut hm = HashMap::new();
-    for (key, value) in vec{
-        hm.insert(key, value);
+    for val in v1_iter{
+        println!("Got: {val}")
     }
-    return hm; 
-}
- 
- fn main(){
-    let input_vec = vec![(String::from("Tarun"), 18), (String::from("Tarun"), 18)];
-    let hm = group_value_by_key(input_vec);
-    print!("{:?}", hm)
+    println!("{:?}", v1)
 }
