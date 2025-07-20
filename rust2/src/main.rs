99e7b36e7c9ae44a3a7c7d7af237e4cc23b562b0
Tarun sharma <tarunsharmawee@gmail.com>
2025-07-20 17:01:22 +0530

    vectors

--- rust2/src/main.rs
+++ rust2/src/main.rs
@@ -3,6 +3,21 @@ fn main () {
     vec.push(1);
     vec.push(2);
     vec.push(3);
-
-    print!("{:?}",vec);
+    vec.push(4);
+    vec.push(5);
+    vec.push(6);
+    println!("{:?}",vec);
+    let even_val = even_val_vector(vec);
+    println!("{:?}",even_val);
+    
 }
+fn even_val_vector(vec: Vec<usize>)-> Vec<usize>{
+    let mut new_vec = Vec::new();
+
+    for i in vec[0]..vec.len() + 1{
+        if i % 2 == 0 {
+            new_vec.push(i);
+        }
+    }
+    return new_vec;
+}   
