2a0e7c349d2d8387603e3c80e461ea1e07ae1baf
Tarun sharma <tarunsharmawee@gmail.com>
2025-07-21 17:53:37 +0530

    Filter

--- rust2/src/main.rs
+++ rust2/src/main.rs
@@ -2,7 +2,7 @@ fn  main(){
     let v1 = vec![1,2,3];
     let v1_iter = v1.iter();
 
-    let v1_iter2 = v1_iter.map(|x| x + 1);
+    let v1_iter2 = v1_iter.filter(|x| *x % 2 == 0);
 
     for i in v1_iter2 {
         print!("{i}")
