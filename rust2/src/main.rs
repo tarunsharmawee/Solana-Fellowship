7f86ed764b5ee933a2419a6c4414f323e29cfd7b
Tarun sharma <tarunsharmawee@gmail.com>
2025-07-21 18:06:05 +0530

    itretor quiz

--- rust2/src/main.rs
+++ rust2/src/main.rs
@@ -2,10 +2,10 @@ fn  main(){
     let v1 = vec![1,2,3];
     let v1_iter = v1.iter();
 
-    let v1_iter2 = v1_iter.filter(|x| *x % 2 == 0);
+    let v1_iter2 = v1_iter.filter(|x| *x % 2 == 1);
+    let v1_iter3 = v1_iter2.map(|x| x*2); 
 
-    for i in v1_iter2 {
-        print!("{i}")
-    }
-    println!("{:?}", v1)
+    let v2: Vec<i32> = v1_iter3.collect();  
+
+    println!("{:?}", v2)
 }  
