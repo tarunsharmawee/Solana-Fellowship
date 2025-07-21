b1c611664a3d26d3592d95043075296e176baedc
Tarun sharma <tarunsharmawee@gmail.com>
2025-07-21 23:03:55 +0530

    String Slices

--- rust2/src/main.rs
+++ rust2/src/main.rs
@@ -1,11 +1,8 @@
-fn  main(){
-    let v1 = vec![1,2,3];
-    let v1_iter = v1.iter();
+use std::option;
 
-    let v1_iter2 = v1_iter.filter(|x| *x % 2 == 1);
-    let v1_iter3 = v1_iter2.map(|x| x*2); 
-
-    let v2: Vec<i32> = v1_iter3.collect();  
-
-    println!("{:?}", v2)
-}  
+fn main(){
+    let word = String::from("Tarun Sharma");   
+    let word2 = &word[0..5];
+    println!("{word2}")
+    
+}
