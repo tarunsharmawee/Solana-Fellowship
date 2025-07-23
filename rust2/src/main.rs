2b4e86fabea785e1d8ac05776eff8cd1f40004da
Tarun sharma <tarunsharmawee@gmail.com>
2025-07-23 14:49:07 +0530

    lifetimes with struct

--- rust2/src/main.rs
+++ rust2/src/main.rs
@@ -1,17 +1,13 @@
-fn main() {
-    let ans;
-    let str1 = String::from("small");
-    {
-        let str2 = String::from("longer");
-        ans = longest(&str1, &str2); // this is giving error coz it's lifetime ends with the scope so u cant use it afterwardsgit a
-    }
-    print!("longer string is : {ans}")
+struct User<'a> {
+    name: &'a str,
 }
 
-fn longest<'a>(str1: &'a str, str2: &'a str) -> &'a str {
-    if str2.len() > str2.len() {
-        return str2;
-    } else {
-        return str2;
+fn main() {
+    let user;
+    {
+        let name = String::from("Tarun");
+        user = User { name: &name };
     }
+
+    print!("{}", user.name)
 }
